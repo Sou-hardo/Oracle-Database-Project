1b1df92cc27055b6ed7667fe27635c325ee24be9
Sou-hardo <116086139+Sou-hardo@users.noreply.github.com>
2023-07-23 21:35:29 +0600

    Data for Book table has been inserted

---
+++ books_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Book VALUES (1, 'Harry Potter and the Sorcerer''s Stone', TO_DATE('26-JUN-1997', 'DD-MON-YYYY'), 19.99, 100);
+INSERT INTO Book VALUES (2, 'A Game of Thrones', TO_DATE('06-AUG-1996', 'DD-MON-YYYY'), 24.95, 75);
+INSERT INTO Book VALUES (3, 'It', TO_DATE('15-SEP-1986', 'DD-MON-YYYY'), 18.50, 120);
+INSERT INTO Book VALUES (4, 'Murder on the Orient Express', TO_DATE('01-JAN-1934', 'DD-MON-YYYY'), 12.99, 50);
+INSERT INTO Book VALUES (5, 'The Old Man and the Sea', TO_DATE('01-SEP-1952', 'DD-MON-YYYY'), 9.95, 80);
+
+Select * from Book;
